64add78696c8fd255d099188a7f787d6cba741c3
Adrian-89 <adrianif2003@gmail.com>
2024-04-18 18:54:07 +0100

    Se sube plane.py

---
+++ plane.py
@@ -0,0 +1,28 @@
+class Plane:
+    def __init__(self,
+        gates: int, 
+        total_fuel: float,
+        fuel_comsumption: float,
+        plane_model: str,
+        pasengers_capacity: int,
+        engine: bool = True,
+        window_position: bool = True,
+        plane_wheels: bool = True,
+        plane_fly: bool = True,
+        travel_km: int
+):
+
+def num_gates(self):
+    self.gates = 6
+
+def total_fuel(self):
+    self.total_fuel = 100.0
+
+def fuel_comsumption(self, comsumption: float):
+    comsumption * self.travel_km
+
+def travel_km(self):
+
+
+
+
